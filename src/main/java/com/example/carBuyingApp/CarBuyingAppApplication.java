c325dba7f484e383e3b7dfa4e2ba68fb9c1fc2e7
Ehab Ahmed <ehab.ahmed.yassen@gmail.com>
2025-12-01 18:21:28 +0200

    initial commit
    * create project structure

---
+++ src/main/java/com/example/carBuyingApp/CarBuyingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.carBuyingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBuyingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBuyingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carBuyingApp/CarBuyingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carBuyingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBuyingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
